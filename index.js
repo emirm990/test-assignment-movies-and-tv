c4aebe660b67c527fec59a16066739664888c1fa
emirm990 <emir.mekic@live.com>
2018-12-14 13:36:27 +0100

    added trailers
    Details view shows trailers if they are available

--- index.js
+++ index.js
@@ -14,24 +14,39 @@ let span = document.getElementsByClassName("close")[0];
 let modalContent = document.getElementById("modal-content");
 let documentTitle = document.getElementById("title");
 
+function mainContent(id, poster, name){
+    return movies.innerHTML += 
+    `<div id=${id} class="card col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
+        <img class="card-img-top" src="http://image.tmdb.org/t/p/w400//${poster}">
+        <h5 class="card-title text-center mt-3">${name}</h5>
+    </div>`
+}
+function detailsContent(poster, name, overview){
+    return modalContent.innerHTML = 
+        `<img class="card-img-top img-fluid" src="http://image.tmdb.org/t/p/w400/${poster}">
+        <h4 class="card-title text-center mt-3">${name}</h4>
+        <p class="card-text px-3 pb-2">${overview}</p>`
+}
+function detailsContentTrailer(trailer, name, overview){
+    let video = document.getElementById("video");
+    return modalContent.innerHTML = 
+        `<div class="videoWrapper">
+            <iframe id="video" width="560" height="315" src="https://www.youtube.com/embed/${trailer}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>    
+        </div>
+        <h4 class="card-title text-center mt-3">${name}</h4>
+        <p class="card-text px-3 pb-2">${overview}</p>`
+}
 async function getData(tvOrMovie){
     let response = await fetch("https://api.themoviedb.org/3/" + tvOrMovie + "/top_rated?api_key="+apiKey+"&language=en-US&page=1");
     let json = await response.json();
+    
     if (tvButtonClicked){
         for (let i=0;i<10;i++){
-            movies.innerHTML += 
-                `<div id=${json.results[i].id} class="card col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                    <img class="card-img-top" src="http://image.tmdb.org/t/p/w400//${json.results[i].poster_path}">
-                    <h5 class="card-title text-center mt-3" id="${i}">${json.results[i].name}</h5>
-                </div>`
+            mainContent(json.results[i].id, json.results[i].poster_path, json.results[i].name);
         }
     }else{
         for (let i=0;i<10;i++){
-            movies.innerHTML += 
-                `<div id=${json.results[i].id} class="card col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                    <img class="card-img-top" src="http://image.tmdb.org/t/p/w400//${json.results[i].poster_path}">
-                    <h5 class="card-title text-center mt-3" id="${i}">${json.results[i].title}</h5>
-                </div>`
+            mainContent(json.results[i].id, json.results[i].poster_path, json.results[i].title);
         }
     }
 }
@@ -41,47 +56,54 @@ async function getSearch(searchInput){
     }else tvOrMovie = "movie";
     let response = await fetch("https://api.themoviedb.org/3/search/" + tvOrMovie + "?api_key=fdcceaee503d65d10f646f384fbc9aec&language=en-US&query=" + searchInput + "&page=1&include_adult=false");
     let json = await response.json();
-
+    
     if (tvButtonClicked){
         for (let i=0;i<10;i++){
-            movies.innerHTML += 
-                `<div id=${json.results[i].id} class="card col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                    <img class="card-img-top" src="http://image.tmdb.org/t/p/w400//${json.results[i].poster_path}">
-                    <h5 class="card-title text-center mt-3" id="${i}">${json.results[i].name}</h5>
-                </div>`
+            mainContent(json.results[i].id,json.results[i].poster_path,json.results[i].name);
         }
     }else{
         for (let i=0;i<10;i++){
-            movies.innerHTML += 
-                `<div id=${json.results[i].id} class="card col-6 col-sm-6 col-md-4 col-lg-3 mb-3">
-                    <img class="card-img-top" src="http://image.tmdb.org/t/p/w400//${json.results[i].poster_path}">
-                    <h5 class="card-title text-center mt-3" id="${i}">${json.results[i].title}</h5>
-                </div>`
+            mainContent(json.results[i].id,json.results[i].poster_path,json.results[i].title);
         }
     }
     
 }
 
-async function getById(id, type){
+async function getById(id){
     let response;
     let json;
+    let responseVideo;
+    let videoJson;
+    
     if (tvButtonClicked){
         response = await fetch("https://api.themoviedb.org/3/tv/" + id + "?api_key=" + apiKey + "&language=en-US");
         json = await response.json();
-        modalContent.innerHTML = 
-        `<img class="card-img-top img-fluid" src="http://image.tmdb.org/t/p/w400/${json.poster_path}">
-        <h4 class="card-title text-center mt-3">${json.name}</h4>
-        <p class="card-text px-3 pb-2">${json.overview}</p>`
-    }else {
+        responseVideo = await fetch("https://api.themoviedb.org/3/tv/" + id + "/videos?api_key=" + apiKey + "&language=en-US");
+        videoJson = await responseVideo.json();
+        if (videoJson.results != 0){
+            console.log(videoJson.results);
+            for (index in videoJson.results){
+                
+                if(videoJson.results[index].type == "Trailer"){
+                    detailsContentTrailer(videoJson.results[0].key, json.name, json.overview);
+                } else detailsContent(json.poster_path, json.name, json.overview);
+            }
+        }else detailsContent(json.poster_path, json.name, json.overview);   
+    }else{
         response = await fetch("https://api.themoviedb.org/3/movie/" + id + "?api_key=" + apiKey + "&language=en-US");
         json = await response.json();
-        modalContent.innerHTML = 
-        `<img class="card-img-top img-fluid" src="http://image.tmdb.org/t/p/w400/${json.poster_path}">
-        <h4 class="card-title text-center mt-3">${json.title}</h4>
-        <p class="card-text px-3 pb-2">${json.overview}</p>`
+        responseVideo = await fetch("https://api.themoviedb.org/3/movie/" + id + "/videos?api_key=" + apiKey + "&language=en-US");
+        videoJson = await responseVideo.json();
+        if (videoJson.results != 0){
+            for (index in videoJson.results){
+                if(videoJson.results[index].type == "Trailer"){
+                    detailsContentTrailer(videoJson.results[0].key, json.title, json.overview);
+                } //else detailsContent(json.poster_path, json.title, json.overview);
+            }
+        }else detailsContent(json.poster_path, json.title, json.overview);   
+        //detailsContent(json.poster_path, json.title, json.overview);
     }
-}
-
+    }
 search.addEventListener("input", function(){
     movies.innerHTML = "";
     if (search.value.length > 2){
@@ -102,6 +124,7 @@ moviesButton.addEventListener("click", function(){
     tvOrMovie = "movie";
     movies.innerHTML = "";
     documentTitle.innerHTML = "Movies";
+    search.value = '';
     getData("movie"); 
 });
 
@@ -112,20 +135,24 @@ tvButton.addEventListener("click", function(){
     tvButtonClicked = true;
     movies.innerHTML = "";
     documentTitle.innerHTML = "TV";
+    search.value = '';
     getData("tv");
 })
 
 movies.addEventListener("click", function(event){
     modal.style.display = "block";
     let id = event.target.parentElement.id;
+    console.log(id);
     getById(id);
 })
 span.onclick = function() {
     modal.style.display = "none";
+    video.src = video.src;
 }
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    video.src = video.src;
   }
 }
 getData("tv");
